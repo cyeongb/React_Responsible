092d6ec99e0dd310f0eddd49df11a62af6a37905
choeyeongbi <cyeongb@gmail.com>
2021-12-22 00:05:53 +0900

    update react-router-dom about version6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Switch is replaced by 'Routes'
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
